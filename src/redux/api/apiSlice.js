3248b4302625799fb2c9318e86fa927af5167bab
root <root@DESKTOP-OSR5U79>
2024-11-25 15:51:20 +0600

    [ADDD]]

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -5,7 +5,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://192.168.10.168:4040/api/v1",
+    // baseUrl: "http://192.168.10.168:4040/api/v1",
+    baseUrl: "http://167.99.11.156:4040/api/v1",
     prepareHeaders: (headers, { getState }) => {
       const token = localStorage.getItem("token");
     //   console.log("9 baseApi", token);
--- src/redux/api/baseUrl.js
+++ src/redux/api/baseUrl.js
@@ -1,3 +1,3 @@
 
-const url = "http://192.168.10.168:4040"
+const url = "http://167.99.11.156:4040"
 export default url
