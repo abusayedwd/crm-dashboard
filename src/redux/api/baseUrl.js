2ea1788656c3490d2f3b4e51b6dd4e1f8c1afd68
root <root@DESKTOP-OSR5U79>
2025-03-18 15:38:20 +0600

    fix changes

--- src/redux/api/baseUrl.js
+++ src/redux/api/baseUrl.js
@@ -1,4 +1,5 @@
 
 
-const url = "https://api.lenbec.com"
+// const url = "https://api.lenbec.com"
+const url = "http://10.0.60.205:4040"
 export default url
--- src/redux/features/paymentInRevolut/getpayment.js
+++ src/redux/features/paymentInRevolut/getpayment.js
@@ -6,7 +6,7 @@ import { apiSlice } from "../../api/apiSlice";
 const revolutPayment = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         revolutPayment: builder.query({
-            query: () => `/payment-invoice/getAllRevaluePayments`,
+            query: () => `/payment-invoice/getRevoultPayments`,
             providesTags: [{type: "Revolut"}]
         })
     })
